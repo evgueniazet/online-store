0f3d7d7ea0e13f201507c391a5180b2c3d22e192
evgueniazet <zelenkoea19@gmail.com>
2022-12-16 12:48:50 +0100

    add: scss configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './App.css'
+import styles from './App.module.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      Awsome store app!!!
+    <div className={styles.app}>
+      Awesome store app!!!
     </div>
   )
 }
