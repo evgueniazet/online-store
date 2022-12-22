449f99feddcb2c7ed72051ae5aef5abe1e965daa
evgueniazet <zelenkoea19@gmail.com>
2022-12-22 12:18:22 +0100

    refactor: change structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
-import React, { useState } from 'react';
-import styles from './App.module.scss';
-import './styles/fonts.scss';
-import { Button } from './components/Button/Button';
-import { ButtonColors } from './enums/ButtonColors';
-import { TextInput } from './components/Input/TextInput';
-import { Checkbox } from './components/Checkbox/Checkbox';
+import { Sendbox } from './components/Sendbox/Sendbox';
 
 const App = () => {
-  const [value, setValue] = React.useState('123');
-  const [isCheckedA, setIsCheckedA] = useState(false);
-  const [isCheckedB, setIsCheckedB] = useState(false);
-
-  const handleClick = (): void => {
-    console.log('click');
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue(e.target.value);
-  };
-
-  const handleChangeA = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsCheckedA(e.target.checked);
-  };
-
-  const handleChangeB = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsCheckedB(e.target.checked);
-  };
-
-  return (
-    <div className={styles.app}>
-      Awesome store app!!!
-      <Button title='button1' color={ButtonColors.Primary} onClick={handleClick} />
-      <Button title='button2' color={ButtonColors.Secondary} onClick={handleClick} />
-      <TextInput placeholder='Insert text' onChange={handleChange} value={value} />
-      <Checkbox handleChange={handleChangeA} isChecked={isCheckedA} label='A' />
-      <Checkbox handleChange={handleChangeB} isChecked={isCheckedB} label='B' />
-    </div>
-  );
+  return <Sendbox />;
 };
 
 export default App;
