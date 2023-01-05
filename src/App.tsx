cd6b6f5344c5b37c80706e9894ffaeed32194e45
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-01-05 17:53:18 +0200

    feat: add local data copy and and data loading to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/fonts.scss';
 import Route from './utils/Route';
 import HomePage from './components/Pages/HomePage';
 import CheckoutPage from './components/Pages/CheckoutPage';
 import ProductPage from './components/Pages/ProductPage';
 import { Sandbox } from './components/Sandbox/Sandbox';
+import DataService from './utils/DataService';
+import productsData from './data';
 
 const App = () => {
 
+  const dataService: DataService = DataService.getInstance();
+  const [isDataLoaded, setIsDataLoaded] = useState<boolean>(false);
+  const fetchData = () => {
+    fetch('https://dummyjson.com/products?limit=100')
+      .then((response) => response.json())
+      .then((data) => {
+        dataService.initialiseData(data.products);
+        setIsDataLoaded(true);
+      }).catch((e) => {
+        // backup plan: load data from local file
+        dataService.initialiseData(productsData);
+        setIsDataLoaded(true);
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log('init app');
+  }, []);
+
   return (
     <div>
-      <Route path="/">
-        <HomePage />
-      </Route>
-      <Route path="/checkout">
-        <CheckoutPage />
-      </Route>
-      <Route path="/product">
-        <ProductPage />
-      </Route>
-      <Route path="/sandbox">
-        <Sandbox />
-      </Route>
-
+      {!isDataLoaded ? (
+        'Loading...'
+      ) : (
+        <div>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/checkout'>
+            <CheckoutPage />
+          </Route>
+          <Route path='/product'>
+            <ProductPage />
+          </Route>
+          <Route path='/sandbox'>
+            <Sandbox />
+          </Route>
+        </div>
+      )}
     </div>
   );
 };
